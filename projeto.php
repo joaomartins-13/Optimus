7e66649e0b188ccaf376eca93298b8a70dc52048
Joao Martins de Souza <88095023+joaomartins-13@users.noreply.github.com>
2023-03-20 12:35:13 -0300

    Add files via upload

---
+++ projeto.php
@@ -0,0 +1,68 @@
+<?php
+    include "include/menu.php";
+?>
+
+ <br><br><br><br>
+
+    <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light" data-aos="fade-down" data-aos-delay="200" data-aos-duration="600">
+      <div class="col-md-5 p-lg-5 mx-auto my-5">
+        <h1 class="display-4 font-weight-normal">Um pouquinho sobre nossos projetos</h1>
+        <p class="lead font-weight-normal">Nossa equipe participou de desafios emocionantes e queremos compartilhar nossas experiências e projetos.</p>
+      </div>
+      <div class="product-device shadow-sm d-none d-md-block"></div>
+      <div class="product-device product-device-2 shadow-sm d-none d-md-block"></div>
+    </div>
+
+    <div class="d-md-flex flex-md-equal w-100 my-md-3 pl-md-3" >
+      <div class="bg-darki mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden" data-aos="fade-up" data-aos-delay="200" data-aos-duration="600">
+        <div class="my-3 py-3">
+          <h2 class="display-5">Kart</h2>
+          <p class="lead">E outra descrição engraçadinha.</p>
+        </div>
+        <div class="bg-light shadow-sm mx-auto" style="width: 100%; height: 200px; border-radius: 21px 21px 0 0;">
+        <img src="assets/images/nosso_kart.JPG" alt="Client 1" width="400" height="400">
+        </div>
+      </div>
+      <div class="bg-redi mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden"  data-aos="fade-right" data-aos-delay="200" data-aos-duration="600">
+        <div class="my-3 p-3"> <h2 class="display-5">Rolimã</h2>
+          <p class="lead">E outra descrição mais engraçadinha ainda.</p>
+        </div>
+        <div class="bg-redi shadow-sm mx-auto banner2" style="width: 100%; height: 300px; border-radius: 21px 21px 0 0;">
+        <img src="assets/images/nosso_kart.JPG" alt="Client 1" width="400" height="300">
+        </div>
+      </div>
+    </div>
+
+
+
+      <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+      <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+      <script src="../../assets/js/vendor/popper.min.js"></script>
+      <script src="../../dist/js/bootstrap.min.js"></script>
+      <script src="../../assets/js/vendor/holder.min.js"></script>
+      <script>
+        Holder.addTheme('thumb', {
+          bg: '#55595c',
+          fg: '#eceeef',
+          text: 'Thumbnail'
+        });
+      </script>    
+      </section>
+
+      <?php
+        include "include/rodape.php";
+      ?>
+
+    <!--jQuery-->
+    <script src="assets/js/jquery-3.3.1.js"></script>
+    <!--Plugins-->
+    <script src="assets/js/bootstrap.bundle.js"></script>
+    <script src="assets/js/loaders.css.js"></script>
+    <script src="assets/js/aos.js"></script>
+    <script src="assets/js/swiper.min.js"></script>
+    <script src="assets/js/lightgallery-all.min.js"></script>
+    <!--Template Script-->
+    <script src="assets/js/main.js"></script>
+ </body>
+
+</html>
