2ea1f1de241648f9189544d7bcacc10bfc259aec
Joao Martins de Souza <88095023+joaomartins-13@users.noreply.github.com>
2023-04-03 12:34:04 -0300

    Add files via upload

---
+++ optimus/include/rodape.php
@@ -0,0 +1,80 @@
+<footer>
+        <!-- Widgets Start -->
+        <div class="footer-widgets">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 col-xl-3">
+                        <div class="single-widget contact-widget" data-aos="fade-up" data-aos-delay="0">
+                            <h6 class="widget-tiltle">Contatos</h6>
+                            <div class="media">
+                                <i class="fa fa-map-marker"></i>
+                                <div class="media-body ml-3">
+                                    <h6>Endereço</h6>
+                                    Alameda Ingá, Quadra B,<br>
+                                    s/n - Campus Samambaia<br>
+                                    , CEP 74045-155 Goiânia-GO<br>
+                                    IMEC
+                                </div>
+                            </div>
+                            <div class="media">
+                                <i class="fa fa-envelope-o"></i>
+                                <div class="media-body ml-3">
+                                    <h6>Tem alguma duvida?</h6>
+                                    <a href="mailto:support@steelthemes.com">Suporte@optimus.ufg.br</a>
+                                </div>
+                            </div>
+                            <div class="media">
+                                <i class="fa fa-phone"></i>
+                                <div class="media-body ml-3">
+                                    <h6>Celular</h6>
+                                    <a href="tel:+610791803458"> +55 (62) 999999999</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-xl-3">
+
+                    </div>
+                    <div class="col-md-6 col-xl-3">
+
+                    </div>
+                    
+                    <div class="col-md-6 col-xl-3">
+                        <div class="single-widget tags-widget" data-aos="fade-up" data-aos-delay="800">
+                            <h6 class="widget-tiltle">Popular Tags</h6>
+                            <a href="#">Optimus</a>
+                            <a href="#">Formula_sae</a>
+                            <a href="#">TOO</a>
+                            <a href="#">Melhor Projeto da UFG</a>
+                            <a href="#">Viagem</a>
+                            <a href="#">Chassi</a>
+                        </div>
+                        <div class="single-widget subscribe-widget" data-aos="fade-up" data-aos-delay="800">
+                            <h6 class="widget-tiltle">Siga a gente nas redes sociais</h6>
+                            <p></p>
+                            <ul class="nav social-nav">
+                                <li><a href="https://www.facebook.com/OptimusUfg" target="_blank"><i class="fa fa-facebook"></i></a></li>
+                                <li><a href="https://www.youtube.com/@optimusfsaeufg"><i class="fa fa-youtube"></i></a></li>
+                                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                <li><a href="https://www.instagram.com/optimusfsae/"><i class="fa fa-instagram"></i></a></li>
+                                <li><a href="https://www.linkedin.com/company/equipeoptimusufg/mycompany/"><i class="fa fa-linkedin"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Widgets End -->
+        <!-- Foot Note Start -->
+        <div class="foot-note">
+            <div class="container">
+                <div
+                    class="footer-content text-center text-lg-left d-lg-flex justify-content-between align-items-center">
+                    <p class="mb-0" data-aos="fade-right" data-aos-offset="0">&copy;  2023 Todos os direitos reservados. Desenhado por Optimus UFG.</p>
+                </div>
+            </div>
+        </div>
+        <!-- Foot Note End -->
+    </footer>
+</body>
+</html>
---
+++ optimus/patrocinador.php
@@ -0,0 +1,30 @@
+<?php
+    include "include/menu.php";
+?>
+
+ <br><br><br><br>
+    <section class="services">
+ <div class="container marketing">
+
+
+
+<br><br><br><br><br><br><br><br><br><br><br><br><br>
+ 
+</div>
+    <!-- Footer Endt -->
+    <!--jQuery-->
+    <script src="assets/js/jquery-3.3.1.js"></script>
+    <!--Plugins-->
+    <script src="assets/js/bootstrap.bundle.js"></script>
+    <script src="assets/js/loaders.css.js"></script>
+    <script src="assets/js/aos.js"></script>
+    <script src="assets/js/swiper.min.js"></script>
+    <script src="assets/js/lightgallery-all.min.js"></script>
+    <!--Template Script-->
+    <script src="assets/js/main.js"></script>
+    <?php
+        include "include/rodape.php";
+    ?>
+</body>
+
+</html>
